ccc58b888113943ff7532838a059ba8b636fb997
emily_chen <emilycodeee@gmail.com>
2021-01-24 14:04:27 +0800

    feat : modify index.hbs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exshbs = require('express-handlebars')
 const app = express()
+const Todo = require('./models/todo')
 // 載入 mongoose
 const mongoose = require('mongoose')
 // 連線至mongodb
@@ -21,8 +22,10 @@ app.engine('hbs', exshbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-
-  res.render('index')
+  Todo.find()
+    .lean()
+    .then(todos => res.render('index', { todos }))
+    .catch(error => console.error(error))
 })
 
 app.listen(3000, () => {
