f027c1da82f28dee831a4765299ac579809c4a4c
emily_chen <emilycodeee@gmail.com>
2021-01-24 09:43:08 +0800

    feat : add mongoose

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const express = require('express')
 const app = express()
+// 載入 mongoose
+const mongoose = require('mongoose')
+// 連線至mongodb
+// 執行至此行就會與資料庫連線
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
 
---
+++ models/todo.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const todoSchema = new Schema({
+
+  name: {
+    type: String,
+    requires: true
+  }
+
+})
+
+module.exports = mongoose.model('Todo', todoSchema)
