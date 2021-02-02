ce1d873bdd0c32062f6dcebe8fe63ff5ba2883e7
emily_chen <emilycodeee@gmail.com>
2021-02-02 19:07:21 +0800

    chore:heroku init

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 // 引用路由器
 const routes = require('./routes')
+const PORT = process.env.PORT || 3000
 // 將request 導入路由器
 // 默認會去找/routes目錄下index的檔案
 
@@ -19,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
 
-app.listen(4000, () => {
+app.listen(PORT, () => {
 
-  console.log('express is running on http://localhost:3000')
+  console.log(`express is running on http://localhost:${PORT}`)
 })
--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/todo-list'
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 
 db.on('error', () => {
